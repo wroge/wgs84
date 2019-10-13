f125d2e3fb825d4d70e3b96f75e2bf48b584b79c
Malte Wrogemann <malte.wrogemann@t-online.de>
2019-10-13 22:15:09 +0200

    v0.4.9

--- epsg_test.go
+++ epsg_test.go
@@ -1,21 +1,27 @@
 package wgs84_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/wroge/wgs84"
 )
 
 func Test(t *testing.T) {
-	epsg := wgs84.EPSG()
-	var from = 4326
-	var a, b, c = 9.0, 52.0, 0.0
-	for _, code := range epsg.CodesCover(a, b) {
-		a, b, c = wgs84.Transform(epsg.Code(from), epsg.Code(code))(a, b, c)
-		from = code
-	}
-	a, b, c = wgs84.Transform(epsg.Code(from), epsg.Code(4326)).Round(0)(a, b, c)
-	if a != 9 || b != 52 || c != 0 {
-		t.Fatal(a, b, c)
+	for lon := -179.0; lon < 180; lon += 1.5 {
+		for lat := -89.0; lat < 90; lat += 1.5 {
+			epsg := wgs84.EPSG()
+			var from = 4326
+			var a, b, c = lon, lat, 0.0
+			for _, code := range epsg.CodesCover(lon, lat) {
+				a, b, c = wgs84.Transform(epsg.Code(from), epsg.Code(code))(a, b, c)
+				from = code
+			}
+			a, b, c = wgs84.Transform(epsg.Code(from), epsg.Code(4326)).Round(3)(a, b, c)
+			if a != lon || b != lat {
+				fmt.Println(lon, lat, a, b)
+				t.Fatal(a, b, c)
+			}
+		}
 	}
 }
